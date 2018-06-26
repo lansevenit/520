38669b60a3da812a43745daac927d85941f3a04a
lansevenit <leonardo.mendes@4linux.com.br>
2018-06-25 22:11:06 -0300

    aula1

---
+++ hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print('Olá Mundo!')
+mensagem = 'hello world'
+nome = input("digite seu nome:")
+print(mensagem, nome)
+idade = int(input('digite a sua idade:'))
+print(mensagem, nome, idade)
+type(idade)
---
+++ media.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+nome = input("Digite o nome do Aluno:")
+nota1 = int(input("Digite a primeira nota: \n"))
+nota2 = int(input("Digite a segunda nota:  \n")) 
+media  = (nota1 + nota2) / 2
+print(" O aluno {} tem a media {}".format(aluno, media))
